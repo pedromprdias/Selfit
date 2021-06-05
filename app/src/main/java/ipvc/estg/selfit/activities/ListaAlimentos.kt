f16104d8caf739da427047d9563e7dbd2e8c548b
leonardofranco98 <61205293+leonardofranco98@users.noreply.github.com>
2021-06-05 13:04:26 +0100

    final login commit

---
+++ app/src/main/java/ipvc/estg/selfit/activities/ListaAlimentos.kt
@@ -0,0 +1,12 @@
+package ipvc.estg.selfit.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ipvc.estg.selfit.R
+
+class ListaAlimentos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_alimentos)
+    }
+}
